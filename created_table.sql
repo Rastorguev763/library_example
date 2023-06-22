16ea2aaa8c51fc10b7fce085a763029ac4cc82ad
Rastorguev763 <alex.rastorguev@bk.ru>
2023-06-22 22:38:50 +0300

    fix .sql and add info in readme

--- created_table.sql
+++ created_table.sql
@@ -1,28 +1,28 @@
-CREATE TABLE Readers (
-  ReaderID INT PRIMARY KEY,
-  Name VARCHAR(255),
-  Surname VARCHAR(255),
-  Patronymic VARCHAR(255),
-  Address VARCHAR(255),
-  Phone VARCHAR(20),
-  BookCode INT,
-  FOREIGN KEY (BookCode) REFERENCES Books (BookCode)
+CREATE TABLE public.Publishers (
+  PublisherID SERIAL PRIMARY KEY,
+  PublisherName VARCHAR(255),
+  City VARCHAR(255)
 );
 
-CREATE TABLE Books (
-  BookCode INT PRIMARY KEY,
+CREATE TABLE public.Books (
+  BookCode SERIAL PRIMARY KEY,
   Title VARCHAR(255),
   Authors VARCHAR(255),
   PublicationYear INT,
   Pages INT,
   Price DECIMAL(10, 2),
   Quantity INT,
-  PublisherID VARCHAR(255),
-  FOREIGN KEY (PublisherID) REFERENCES Publishers (PublisherID)
+  PublisherID INT,
+  FOREIGN KEY (PublisherID) REFERENCES public.Publishers (PublisherID)
 );
 
-CREATE TABLE Publishers (
-  PublisherID VARCHAR(255) PRIMARY KEY,
-  PublisherName VARCHAR(255),
-  City VARCHAR(255)
+CREATE TABLE public.Readers (
+  ReaderID SERIAL PRIMARY KEY,
+  Name VARCHAR(255),
+  Surname VARCHAR(255),
+  Patronymic VARCHAR(255),
+  Address VARCHAR(255),
+  Phone VARCHAR(20),
+  BookCode INT,
+  FOREIGN KEY (BookCode) REFERENCES public.Books (BookCode)
 );
