1879018f46f5672cf20b62c49e81a9597f9fc99a
Rastorguev763 <117032024+Rastorguev763@users.noreply.github.com>
2023-07-10 14:23:33 +0300

    update

--- created_table_lostbook.sql
+++ created_table_lostbook.sql
@@ -2,7 +2,7 @@ CREATE TABLE public.LostBooks (
   LostBooksID SERIAL PRIMARY KEY,
   DateLost DATE,
   BookCode INT,
-  ReaderID INT
-  FOREIGN KEY (BookCode) REFERENCES public.Books (BookCode)
+  ReaderID INT,
+  FOREIGN KEY (BookCode) REFERENCES public.Books (BookCode),
   FOREIGN KEY (ReaderID) REFERENCES public.Readers (ReaderID)
 );
